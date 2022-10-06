bcb40b839c306ac386ba893d32be808a41eb1598
Tatiana Manakhova <tat.manakhova@gmail.com>
2022-10-06 19:00:46 +0300

    feature: add style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './scss/main.scss'; 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".slider .swiper", {
+  slidesPerView: 1.9,
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 200,
+  navigation: {
+    nextEl: ".slider__btn--next",
+    prevEl: ".slider__btn--prev",
+  }
+});
+
+var swiperItems = new Swiper(".choise .swiper", {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".choise__btn--next",
+    prevEl: ".choise__btn--prev",
+  }
+});
+
